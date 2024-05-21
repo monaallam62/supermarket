1b60c86295aa3544ac6be21dbc02f78b760c65bc
monaallam62 <momoallam62@gmail.com>
2024-05-21 15:40:59 +0300

    create project

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+// document.getElementById("div1").innerHTML='mona mohamed allam'
+// document.getElementById("btn1").onmousemove= function(){
+//     document.getElementById("div1").innerHTML="mona allam"
+// }
+// alert("warning")
+// confirm("do you want to login in the website?")
+// console.log("mona")  to developer to check
+// prompt('whats your num?')
+// document.getElementById("btn1").onclick=function(){
+//     document.getElementById("div1").innerHTML="islam mohamed"
+// }
+var input= document.getElementById("input1")
+input.onkeydown=function(){
+    console.log("mona")
+}
